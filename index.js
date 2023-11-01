c3e3d4f77f8c9c44332787194f3b7221e6338a5e
sajib-rana <sajibrana444@gmail.com>
2023-11-01 14:39:26 +0600

    social user

--- index.js
+++ index.js
@@ -9,9 +9,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynhrrkd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +48,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
      const courseCollection = client.db("talent-hut").collection("Courses");
+     const instructorCollection = client.db("talent-hut").collection("Instructor");
+     const reviewCollection = client.db("talent-hut").collection("Reviews");
+     const userCollection = client.db("talent-hut").collection("User");
 
      app.post("/jwt", (req, res) => {
        const user = req.body;
@@ -42,6 +65,58 @@ async function run() {
        const result = await courseCollection.find().toArray();
        res.send(result);
      })
+     app.get("/reviews", async(req, res)=>{
+       const result = await reviewCollection.find().toArray();
+       res.send(result);
+     })
+     app.get("/instructor", async(req, res)=>{
+       const result = await instructorCollection.find().toArray();
+       res.send(result);
+     })
+
+     app.post("/users", async (req, res) => {
+       const user = req.body;
+       const query = { email: user.email };
+       const existingUser = await userCollection.findOne(query);
+
+       if (existingUser) {
+         return res.send({ message: "user already exists" });
+       }
+       const result = await userCollection.insertOne(user);
+       res.send(result);
+     });
+
+      app.get("/users", verifyJWT,  async (req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      });
+
+     app.patch("/users/admin/:id", async (req, res) => {
+       const id = req.params.id;
+       console.log(id);
+       const filter = { _id: new ObjectId(id) };
+       const updateDoc = {
+         $set: {
+           role: "admin",
+         },
+       };
+
+       const result = await userCollection.updateOne(filter, updateDoc);
+       res.send(result);
+     });
+
+    //  app.get("/instructor/:id", async (req, res) => {
+    //    const id = JSON.parse(req.params.id);
+    //    const query = { _id: new ObjectId(id) };
+
+    //    const options = {
+    //      // Include only the `title` and `imdb` fields in the returned document
+    //      projection: { about: 1, _id: 1 },
+    //    };
+
+    //    const result = await instructorCollection.findOne(query, options);
+    //    res.send(result);
+    //  });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
